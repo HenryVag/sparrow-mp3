90c046c748eaf594dbbc4022a5d9fd6a2585f31e
HenryVag <henry.vag@edu.turkuamk.fi>
2026-01-07 11:05:47 +0200

    feat: created index screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,43 @@
-import { Text, View } from "react-native";
+import EmptySongsState from "@/components/EmptySongsState";
+import SongList from "@/components/SongList";
+import { darkTheme, lightTheme } from "@/styles/themes";
+import { useRouter } from "expo-router";
+import { useContext } from "react";
+import { StyleSheet, View } from "react-native";
+import { AppContext } from "../contexts/AppContext";
+
 
 export default function Index() {
+  const appContext = useContext(AppContext);
+  const router = useRouter();
+
+  const theme = appContext?.theme === "dark" ? darkTheme : lightTheme;
+
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={[
+        styles.container,
+        { backgroundColor: theme.background }, // theme applied here
+      ]}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      {appContext?.dirUri ? <SongList /> : <EmptySongsState />}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  header: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  noFolderText: {
+    marginTop: 20,
+    fontSize: 16,
+    textAlign: "center",
+  },
+});

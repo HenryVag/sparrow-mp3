8a4b7116dd0f44f86f06435ff236ea19903d0712
HenryVag <henry.vag@edu.turkuamk.fi>
2026-01-07 11:10:25 +0200

    feat: created player screen

---
+++ app/(tabs)/player.tsx
@@ -0,0 +1,59 @@
+import { NextPrevButton, PlayPauseButton } from "@/components/PlayerButton";
+import { darkTheme, lightTheme } from "@/styles/themes";
+import React, { useContext } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AppContext } from "../contexts/AppContext";
+import { PlayerContext } from "../contexts/PlayerContext";
+
+export default function PlayerScreen() {
+  const playerContext = useContext(PlayerContext);
+  const appContext = useContext(AppContext);
+
+  const theme = appContext?.theme === "dark" ? darkTheme : lightTheme;
+
+  return (
+    <View style={[styles.container, { backgroundColor: theme.background }]}>
+      <Text style={[styles.title, { color: theme.text }]}>
+        {playerContext?.currentSongName}
+      </Text>
+
+      <View style={styles.controls}>
+        <NextPrevButton icon="⏮️" onPress={() => playerContext?.prev()} />
+        <PlayPauseButton
+          playIcon="▶️"
+          pauseIcon="⏸️"
+          onPress={() =>
+            playerContext?.isPlaying
+              ? playerContext?.pause()
+              : playerContext?.play(
+                  playerContext.currentSongURI,
+                  playerContext.currentSongName
+                )
+          }
+        />
+        <NextPrevButton icon="⏭️" onPress={() => playerContext?.next()} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 50,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 40,
+    textAlign: "center",
+  },
+  controls: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    width: "60%",
+  },
+});
